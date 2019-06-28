4568f1f1aade4ca750cc0e3631f9a3facf3f1f43
wangjifeng <wangjifeng@21cnjy.com>
2019-06-28 10:15:58 +0800

    添加项目文件。

---
+++ SwaggerDemo/Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SwaggerDemo.Model;
+
+namespace SwaggerDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+
+        List<Student> stuList = new List<Student>() {
+                new Student{ Id = 1, Name = "张三", Age = 18 },
+                new Student{ Id = 2, Name = "李四", Age = 19 },
+                new Student{ Id = 3, Name = "王五", Age = 20 }};
+
+        /// <summary>
+        /// 学生信息列表
+        /// </summary>
+        /// <remarks>
+        /// 这个api是用来获取学生详细信息列表的接口
+        /// </remarks>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<List<Student>> GetList()
+        {
+            return stuList;
+        }
+
+        /// <summary>
+        /// 学生信息
+        /// </summary>
+        /// <remarks>
+        /// 这个api是用来获取单个学生信息的接口
+        /// </remarks>
+        /// <param name="Id">学生Id</param>
+        /// <returns></returns>
+        [HttpGet("{Id}")]
+        public ActionResult<Student> GetStudent(int Id)
+        {
+            return stuList.Where(s => s.Id == Id).SingleOrDefault();
+        }
+    }
+}
---
+++ SwaggerDemo/Startup.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace SwaggerDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            //注册swagger生成器，定义一个和多个sagger文档
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "My API",
+                    Version = "v1",
+                    Description = "This is WJF Swagger_Api Demo ",
+                    TermsOfService = "None",
+                    Contact=new Contact
+                    {
+                        Name="王继峰",
+                        Email="771862498@qq.com",
+                        Url= "https://www.cnblogs.com/wangjifeng23/"
+                    },
+                    License=new License {
+                        Name="Swagger 许可证",
+                        Url= "https://www.cnblogs.com/wangjifeng23/"
+                    }
+                });
+                // 为 Swagger JSON and UI设置xml文档注释路径
+                var basePath = Path.GetDirectoryName(typeof(Program).Assembly.Location);//获取应用程序所在目录（绝对，不受工作目录影响，建议采用此方法获取路径）
+                var xmlPath = Path.Combine(basePath, "SwaggerDemo.xml");
+                c.IncludeXmlComments(xmlPath);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            //启用中间件服务生成Swagger作为JSON终结点
+            app.UseSwagger();
+            //启用中间件服务对swagger-ui，指定Swagger JSON终结点
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+        }
+    }
+}
